33bf86059afabb5976d7849d2e680196a19929f4
rails -v <c:sites>
2017-04-20 14:36:45 -0500

    sum

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,5 +11,32 @@ class GamesController < ApplicationController
       @outcome = "win"
     end
     render("games/play_rock.html.erb")
+
+  end
+
+  def play_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "paper"
+      @outcome = "tie"
+    elsif @computer_move == "scissors"
+      @outcome - "lose"
+    else
+      @outcome = "win"
+    end
+    render("games/play_paper.html.erb")
+  end
+
+  def play_scissors
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "scissors"
+      @outcome = "tie"
+    elsif @computer_move == "rock"
+      @outcome - "lose"
+    else
+      @outcome = "win"
+    end
+    render("games/play_scissors.html.erb")
   end
 end
