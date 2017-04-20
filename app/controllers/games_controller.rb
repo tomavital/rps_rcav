fbdffc608c9cba8ff6126ba361da0f732353ef70
rails -v <c:sites>
2017-04-20 14:13:53 -0500

    back

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,14 @@
 class GamesController < ApplicationController
 
   def play_rock
-  render("games/play_rock.html.erb")   
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      outcome = "tie"
+    elsif @computer_move == "paper"
+      outcome - "lose"
+    else
+      outcome = "win"
+    end
+    render("games/play_rock.html.erb")
   end
-end
